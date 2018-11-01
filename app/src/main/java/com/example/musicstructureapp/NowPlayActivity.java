599c13add2609f166abecea86a8e1fcd0d77549f
MayankSingh1993 <chaudharysinghmayank00@gmail.com>
2018-11-01 12:24:02 +0530

    onClick attribute is replaced by setOnClickListner.

--- app/src/main/java/com/example/musicstructureapp/NowPlayActivity.java
+++ app/src/main/java/com/example/musicstructureapp/NowPlayActivity.java
@@ -2,152 +2,172 @@ package com.example.musicstructureapp;
 
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class NowPlayActivity extends AppCompatActivity {
+public class NowPlayActivity extends AppCompatActivity implements View.OnClickListener {
 
-
+    private ImageButton playButton, forwardButton, backButton;
     private MediaPlayer mediaPlayer;
     private int pauseCurrentPosition;
     SeekBar seekBar;
     Handler handler;
     Runnable runnable;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_now_play);
-        seekBar=findViewById(R.id.seekBar);
 
-        handler=new Handler();
+        seekBar = findViewById(R.id.seekBar);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.metallica_the_day_that_never_comes);
+
+        playButton = findViewById(R.id.playButton);
+
+        forwardButton = findViewById(R.id.forwardButton);
+
+        backButton = findViewById(R.id.backwardButton);
+
+        TextView textView2 = findViewById(R.id.endDuration);
+
+        final int songDuration = mediaPlayer.getDuration();
+
+        runTimer(songDuration, textView2);
+
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        handler = new Handler();
+
+        playButton.setOnClickListener(this);
+        forwardButton.setOnClickListener(this);
+        backButton.setOnClickListener(this);
+
+        mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                seekBar.setMax(songDuration);
+
+                mediaPlayer.start();
+                changeSeekbar();
+            }
+        });
+
+        //mediaPlayer.setScreenOnWhilePlaying(true);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaPlayer.seekTo(progress);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
 
     }
 
 
-    public void onButtonClick(View view) {
+    private void runTimer(int duration, final TextView textView) {
 
 
-        if(mediaPlayer==null) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.metallica_the_day_that_never_comes);
-            //TextView textView1= findViewById(R.id.startDuration);
-            TextView textView2 = findViewById(R.id.endDuration);
-            final int songDuration = mediaPlayer.getDuration();
-            runTimer(songDuration, textView2);
-            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-
-            /**
-             *
-             */
-            mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                @Override
-                public void onPrepared(MediaPlayer mp) {
-                    seekBar.setMax(songDuration);
-
-                    mediaPlayer.start();
-                    changeSeekbar();
-                }
-            });
-
-            //mediaPlayer.setScreenOnWhilePlaying(true);
-            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-                @Override
-                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                    if(fromUser)
-                    {
-                        mediaPlayer.seekTo(progress);
-                    }
-                }
-
-                @Override
-                public void onStartTrackingTouch(SeekBar seekBar) {
-
-                }
-
-                @Override
-                public void onStopTrackingTouch(SeekBar seekBar) {
-
-                }
-            });
-
-            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mp) {
-                    Toast.makeText(NowPlayActivity.this,"I'm done",Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-        else if(!mediaPlayer.isPlaying()){
-
-            mediaPlayer.seekTo(pauseCurrentPosition);
-            mediaPlayer.start();
-            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                @Override
-                public void onCompletion(MediaPlayer mp) {
-                    Toast.makeText(NowPlayActivity.this,"I'm done",Toast.LENGTH_SHORT).show();
-                }
-            });
-
-        }
-
-
-    }
-
-
-    public void onStopButton(View view) {
-        if (mediaPlayer!=null) {
-            mediaPlayer.stop();
-            mediaPlayer.release();
-            mediaPlayer=null;
-        }
-
-    }
-
-    private void runTimer( int duration, final TextView textView) {
-
-
-
-        duration=duration/1000;
+        duration = duration / 1000;
         int minutes = ((duration) % 3600) / 60;
         int secs = duration % 60;
 
-        @SuppressLint("DefaultLocale") String time = String.format("%02d:%02d",minutes, secs);
+        @SuppressLint("DefaultLocale") String time = String.format("%02d:%02d", minutes, secs);
         textView.setText(time);
 
     }
 
 
-
-    public void onPause(View view)
-    {
-        if(mediaPlayer!=null) {
-            mediaPlayer.pause();
-            pauseCurrentPosition=mediaPlayer.getCurrentPosition();
-        }
-    }
-
-    public void changeSeekbar(){
+    public void changeSeekbar() {
         seekBar.setProgress(mediaPlayer.getCurrentPosition());
-        if(mediaPlayer.isPlaying())
-        {
-            runnable=new Runnable() {
+        if (mediaPlayer.isPlaying()) {
+            runnable = new Runnable() {
                 @Override
                 public void run() {
                     changeSeekbar();
                 }
             };
-            handler.postDelayed(runnable,1000);
+            handler.postDelayed(runnable, 1000);
         }
 
 
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.playButton:
+                if (mediaPlayer.isPlaying()) {
+                    mediaPlayer.pause();
+                    pauseCurrentPosition = mediaPlayer.getCurrentPosition();
+                    playButton.setImageResource(R.drawable.baseline_play_arrow_black_48dp);
+                } else {
+
+                    mediaPlayer.seekTo(pauseCurrentPosition);
+                    mediaPlayer.start();
+                    playButton.setImageResource(R.drawable.baseline_pause_black_48dp);
+                    changeSeekbar();
+                    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                        @Override
+                        public void onCompletion(MediaPlayer mp) {
+                            Toast.makeText(NowPlayActivity.this, "I'm done", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                break;
+            case R.id.forwardButton:
+                mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 5000);
+                break;
+            case R.id.backwardButton:
+                mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() - 5000);
+                break;
+        }
+    }
+
+    public void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+            mediaPlayer.stop();
+      }
+
+
 
 }
--- app/src/main/java/com/example/musicstructureapp/SongActivity.java
+++ app/src/main/java/com/example/musicstructureapp/SongActivity.java
@@ -20,20 +20,18 @@ public class SongActivity extends AppCompatActivity {
 // todo:dynamically from raw song SongName,ArtistName
 
 
-
-
-        songs.add(new Song("Shab Tum Ho",            "Darsan Rawal"));
-        songs.add(new Song("Tera Zikr",              "Darsan Rawal"));
-        songs.add(new Song("Bol Do Na Zara",         "Armaan Malik"));
-        songs.add(new Song("Kaun Tujhe",             "Armaan Malik"));
-        songs.add(new Song("Kuch To Hai",            "Armaan Malik"));
+        songs.add(new Song("Shab Tum Ho", "Darsan Rawal"));
+        songs.add(new Song("Tera Zikr", "Darsan Rawal"));
+        songs.add(new Song("Bol Do Na Zara", "Armaan Malik"));
+        songs.add(new Song("Kaun Tujhe", "Armaan Malik"));
+        songs.add(new Song("Kuch To Hai", "Armaan Malik"));
         songs.add(new Song("Mujhko Barsaat Bana Lo", "Armaan Malik"));
-        songs.add(new Song("Aaj Se Teri",            "Arijit Singh"));
-        songs.add(new Song("Sanam Re",               "Arijit Singh"));
-        songs.add(new Song("Tum Hi Ho",              "Arijit Singh"));
-        songs.add(new Song("Zalima",                 "Arijit Singh"));
-        songs.add(new Song("O Saathi ",              "Atif Aslam"));
-        songs.add(new Song("Tere Sang Yaara ",       "Atif Aslam"));
+        songs.add(new Song("Aaj Se Teri", "Arijit Singh"));
+        songs.add(new Song("Sanam Re", "Arijit Singh"));
+        songs.add(new Song("Tum Hi Ho", "Arijit Singh"));
+        songs.add(new Song("Zalima", "Arijit Singh"));
+        songs.add(new Song("O Saathi ", "Atif Aslam"));
+        songs.add(new Song("Tere Sang Yaara ", "Atif Aslam"));
 
         SongAdapter adapter = new SongAdapter(this, songs);
 
@@ -44,6 +42,7 @@ public class SongActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
 
                 Intent intent = new Intent(SongActivity.this, NowPlayActivity.class);
                 startActivity(intent);
